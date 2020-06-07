45309cf8510aee0213c5129afaf1514dbf462109
Gurubalan-GIT <gurubalan.work@gmail.com>
2020-06-07 22:28:31 +0530

    ✨ feat: (app) added task list ui

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
 import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
+  content,
+  SafeAreaView,
+  FlatList,
   Text,
-  StatusBar,
 } from 'react-native';
+import Header from './components/Header';
+import uuid from 'uuid-random';
+import TaskListItem from './components/TaskListItem';
 
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {id: uuid(), content: 'Finish up projects!'},
+        {id: uuid(), content: 'Attend reviews'},
+        {id: uuid(), content: 'Finish house chores'},
+        {id: uuid(), content: 'Feed the cat!'},
+      ],
+    };
+  }
+
+  deleteTask = (id) => {
+    this.setState({
+      tasks: this.state.tasks.filter((task) => {
+        return task.id !== id;
+      }),
+    });
+  };
+
   render() {
+    const {tasks} = this.state;
     return (
-      <View>
-        <Text>Higfgfg</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Header />
+          <FlatList
+            data={tasks}
+            renderItem={({item}) => (
+              <TaskListItem task={item} deleteTask={this.deleteTask} />
+            )}
+          />
+        </View>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
